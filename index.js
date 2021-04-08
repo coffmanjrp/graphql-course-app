fd00cc41e1a46c98cf1260bfcff811093c04a55c
coffmanjrp <coffmanjrp@gmail.com>
2021-04-07 18:57:58 -0700

    Implement schema and resolvers to GrahpQL settings

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema/schema.js';
+import resolvers from './schema/resolvers.js';
 
 const app = express();
 
 app.get('/', (req, res) =>
   res.send('Go to "/graphql" to launch the GraphQL IDE')
+);
+
+const root = resolvers;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
 );
 
 const PORT = process.env.PORT || 4000;
